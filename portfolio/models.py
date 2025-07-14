dc1734b557dd15c8dd2acc1b184257dc6918fb34
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-14 18:59:38 +0545

    project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+    github_url = models.URLField(blank=True)
+    live_demo = models.URLField(blank=True)
+    thumbnail = models.ImageField(blank=True)
+    tech_stack = models.CharField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        
+        def __str__(self):
+            return self.title
