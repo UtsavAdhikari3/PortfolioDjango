3c62b0ab711455fcba6956d23c7e37cb8e99ff63
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2025-07-18 18:31:13 +0545

    fix static

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
@@ -147,3 +148,5 @@ PORTFOLIO_INTRO = {
     "github": "https://github.com/UtsavAdhikari3",    
     "linkedin": "https://www.linkedin.com/in/utsav-adhikari-762a42235/",
 }
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
